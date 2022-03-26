cdefb253ee6b1eae7d658047d82b828c8a5dda50
marrrrrrme6 <70988388+marrrrrrme6@users.noreply.github.com>
2022-03-26 13:33:55 +0900

    フィルターの追加
    8進数の対応

--- bash-it/themes/d_short/d_short.theme.bash
+++ bash-it/themes/d_short/d_short.theme.bash
@@ -1,5 +1,5 @@
 PS1='[\e[01;32m\u@\h\e[00m \D{%FT %T} :\e[01;34m$(d_short)\e[00m]\n\$ '
-FILTER='sed -e "s/\\\u/$(whoami)/g" -e "s/\\\h/$(hostname)/g" -e 's/$(d_short)/.../g' -e 's/[\\\t\\\T\\\@]/......../g' -e 's/\\\A/...../g' -e 's/\\\$/\$/g' -e 's/[\\\]\\\[]//g'|sed 's/\\\D{\(.*\)}/\1/g' |xargs -i -0 date +"{}" |sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g"'
+FILTER='sed -e "s/\\\u/$(whoami)/g" -e "s/\\\h/$(hostname)/g" -e 's/$(d_short)/.../g' -e 's/[\\\t\\\T\\\@]/......../g' -e 's/\\\A/...../g' -e 's/\\\$/\$/g' -e 's/[\\\]\\\[]//g' -e |sed 's/\\\([0-9]\{3\}\)/\1/g' |xargs -i echo -e "\0{}" |sed 's/\\\D{\(.*\)}/\1/g' |xargs -i -0 date +"{}" |sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g"'
 d_short() {
     DIR=$(pwd | sed "s|$HOME|~|g")
     PROMPT_WIDTH=$(echo -e "$PS1" |$FILTER |xargs)
