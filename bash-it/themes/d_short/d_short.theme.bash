e9215db426216c4480d2cc60ab36393d107efa30
marrrrrrme6 <70988388+marrrrrrme6@users.noreply.github.com>
2022-03-26 21:12:31 +0900

    大幅な修正

--- bash-it/themes/d_short/d_short.theme.bash
+++ bash-it/themes/d_short/d_short.theme.bash
@@ -1,23 +1,28 @@
-PS1='[\e[01;32m\u@\h\e[00m \D{%FT %T} :\e[01;34m$(d_short)\e[00m]\n\$ '
-FILTER='sed -e "s/\\\u/$(whoami)/g" \
--e "s/\\\h/$(hostname)/g" \
--e 's/$(d_short)/.../g' \
--e 's/[\\\t\\\T\\\@]/......../g' \
--e 's/\\\A/...../g' \
--e 's/\\\$/\$/g' \
--e 's/[\\\]\\\[]//g' \
-|sed 's/\\\([0-9]\{3\}\)/\1/g' |xargs -i echo -e "\0{}" \
-|sed 's/\\\D{\(.*\)}/\1/g' |xargs -i -0 date +"{}" \
-|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g"'
-d_short() {
-    DIR=$(pwd | sed "s|$HOME|~|g")
-    PROMPT_WIDTH=$(echo -e "$PS1" |$FILTER |xargs)
-    WIDTH=$(($(tput cols)-${#PROMPT_WIDTH}+2))
-    if  [ ${#DIR} -gt ${WIDTH} ] ;then
-        echo "...${DIR: -$WIDTH}"
-    else
-        echo $DIR
-    fi
+FILTER() {
+    sed \
+    -e 's/\\\$/$/g' \
+    -e 's/\\\n/\n/g' \
+    -e 's/\\A/%H:%M/g' \
+    -e 's/[\\\]\\\[]//g' \
+    -e 's/$(d_short)/.../g' \
+    -e "s/\\\u/$(whoami)/g" \
+    -e "s/\\\h/$(hostname)/g" \
+    |sed 's/\\D{\(.*\)}/\1/g'| xargs -i date +"{}" \
+    |sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" 
 }
-PS1="$PS1"
-FILTER=
+d_short_prompt() {
+    PS1='[\e[01;32m\u@\h\e[00m \D{%FT %T} :\e[01;34m$(d_short)\e[00m]\n\$ '
+    d_short() {
+        DIR=$(pwd | sed "s|$HOME|~|g")
+        PROMPT_WIDTH=$(echo -e "$PS1" |FILTER |xargs)
+        WIDTH=$(($(tput cols)-${#PROMPT_WIDTH}+2))
+        if  [ ${#DIR} -gt ${WIDTH} ] ;then
+            echo "...${DIR: -$WIDTH}"
+        else
+            echo $DIR
+        fi
+    }
+PS1=$PS1
+}
+
+safe_append_prompt_command d_short_prompt
